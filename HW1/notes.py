c7b06a9a7ae33fde14d3c885054e2327ce8f4dd8
unknown <egorchiksv@gmail.com>
2024-07-15 12:59:20 +0300

    В nodes.py создал меню

--- HW1/notes.py
+++ HW1/notes.py
@@ -1,17 +1,22 @@
 while True:
-    com = input("Введите команду:\n1.Показать все заметки\n2.Добавить заметку\n3.Прочитать заметку\n4.Редактировать заметку\n5.Удалить заметку\n6.Выход")
+    com = input("Введите команду:\n1.Показать все заметки\n2.Добавить заметку\n3.Прочитать заметку\n4.Редактировать заметку\n5.Удалить заметку\n6.Выход\n")
     match com:
-        case 1:
-            view_all_notes()
-        case 2:
-            add_notes()
-        case 3:
-            read_note()
-        case 4:
-            edit_note()
-        case 5:
-            del_note()
-        case 6:
+        case "1":
+            print("Введена цифра 1")
+            # view_all_notes()
+        case "2":
+            print("Введена цифра 2")
+            # add_notes()
+        case "3":
+            print("Введена цифра 3")
+            # read_note()
+        case "4":
+            print("Введена цифра 4")
+            # edit_note()
+        case "5":
+            print("Введена цифра 5")
+            # del_note()
+        case "6":
             break
         case _:
-            print("Неверная команда")
+            print("Неверная команда\n")
